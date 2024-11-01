a64dc49da9f86ed1d5addef4c8fd1371acd4fa04
hars123 <harshitshukla4513@gmail.com>
2024-11-01 21:08:13 +0530

    Add

---
+++ Lecture1/Nodeserver.js
@@ -0,0 +1,46 @@
+// console.log("Server file is running ");
+
+// function addtwonumber(a,b){
+//     return a+b;
+// }
+// var result = addtwonumber(4,4);
+// console.log(result);
+
+// // ArrowFunction
+
+// const addtwonumber = (a,b) =>{
+//     return a+b;
+// }
+// var result = addtwonumber(4,4);
+// console.log(result);
+
+// const addtwonumber =(a,b) => a+b;
+
+// callbackfunction
+
+// function callbackfunction(){
+//     console.log("Am a Callbackfunction");
+// }
+
+// const addtwonumber = (a,b) =>{
+//     console.log(a+b);
+    
+//     callbackfunction();
+// }
+
+// addtwonumber(2,3);
+
+
+// const { log } = require('console');
+// var fs = require('fs');
+// var os = require('os');
+
+// var user = os.userInfo();
+// console.log(user.username)
+
+
+// // For creating a File
+// fs.appendFile('greeting.text',user.username + '!', ()=>{
+//     console.log('file is created');
+// });
+
