d19bd8ad44de26f916f66c7cfc9fc182a3e150ee
hars123 <harshitshukla4513@gmail.com>
2024-11-01 21:27:43 +0530

    add

--- Lecture1/Nodeserver.js
+++ Lecture1/Nodeserver.js
@@ -1,46 +1,44 @@
-// console.log("Server file is running ");
+console.log("Server file is running ");
 
-// function addtwonumber(a,b){
-//     return a+b;
-// }
-// var result = addtwonumber(4,4);
-// console.log(result);
+function addtwonumber(a,b){
+    return a+b;
+}
+var result = addtwonumber(4,4);
+console.log(result);
 
-// // ArrowFunction
+// ArrowFunction
 
-// const addtwonumber = (a,b) =>{
-//     return a+b;
-// }
-// var result = addtwonumber(4,4);
-// console.log(result);
+const addtwonumber = (a,b) =>{
+    return a+b;
+}
+var result = addtwonumber(4,4);
+console.log(result);
 
-// const addtwonumber =(a,b) => a+b;
+const addtwonumber =(a,b) => a+b;
+callbackfunction
+function callbackfunction(){
+    console.log("Am a Callbackfunction");
+}
 
-// callbackfunction
-
-// function callbackfunction(){
-//     console.log("Am a Callbackfunction");
-// }
-
-// const addtwonumber = (a,b) =>{
-//     console.log(a+b);
-    
-//     callbackfunction();
-// }
-
-// addtwonumber(2,3);
+const addtwonumber = (a,b) =>{
+    console.log(a+b);
+    callbackfunction();
+}
+addtwonumber(2,3);
 
 
-// const { log } = require('console');
-// var fs = require('fs');
-// var os = require('os');
+const { log } = require('console');
+var fs = require('fs');
+var os = require('os');
 
-// var user = os.userInfo();
-// console.log(user.username)
+var user = os.userInfo();
+console.log(user.username)
 
 
-// // For creating a File
-// fs.appendFile('greeting.text',user.username + '!', ()=>{
-//     console.log('file is created');
-// });
+// For creating a File
+fs.appendFile('greeting.text',user.username + '!', ()=>{
+    console.log('file is created');
+});
 
+const notes = require('./notes.js');
+console.log("NodeserverOpen");
---
+++ Lecture1/notes.js
@@ -0,0 +1,1 @@
+console.log("Notes from Different file");
