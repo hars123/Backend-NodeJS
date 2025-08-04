029a3601daedb5fe4f2ea1467f77c9b7d5d8f246
hars123 <harshitshukla4513@gmail.com>
2025-08-04 20:31:33 +0530

    Add

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Create a server 
+// // Create a server 
+
+// const http = require('http')
+// const server = http.createServer((req,  res)=>{
+//     res.end('Hello am Harshit')
+// })
+
+// server.listen(3000)
+
+
